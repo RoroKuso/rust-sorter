f6373394bb9a7234e7316d481682211953e93bbf
Rodrigue T <tavernier.rodrigue@gmail.com>
2025-07-29 14:45:23 +0200

    added quicksort including custom pivot

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-#[allow(unused)]
+#![allow(unused)]
+
+use std::mem::swap;
+
 fn selection_sort<T: PartialOrd + PartialEq>(slice: &mut [T]) {
     for i in 0..slice.len() {
         let mut ind = i;
@@ -11,7 +14,6 @@ fn selection_sort<T: PartialOrd + PartialEq>(slice: &mut [T]) {
     }
 }
 
-#[allow(unused)]
 fn insertion_sort<T: PartialOrd + PartialEq>(slice: &mut [T]) {
     for i in 1..slice.len() {
         let mut j = i;
@@ -22,7 +24,6 @@ fn insertion_sort<T: PartialOrd + PartialEq>(slice: &mut [T]) {
     }
 }
 
-#[allow(unused)]
 fn bubble_sort<T: PartialOrd + PartialEq>(slice: &mut [T]) {
     for i in 0..slice.len() {
         let mut flag = false;
@@ -36,15 +37,58 @@ fn bubble_sort<T: PartialOrd + PartialEq>(slice: &mut [T]) {
     }
 }
 
+fn simple_pivot<T: PartialOrd + PartialEq>(slice: &[T]) -> usize {
+    (slice.len() + 1) / 2
+}
+
+fn quick_sort<T: PartialOrd + PartialEq>(slice: &mut [T], pivot: fn(&[T]) -> usize) {
+    if slice.len() == 2 {
+        if slice[0] > slice[1] {slice.swap(0, 1);}
+    } else if slice.len() > 2 {
+        let n = slice.len();
+        let p_i = pivot(slice);
+        let nb_less = slice.iter()
+            .filter(|x| **x <= slice[p_i])
+            .count();
+        slice.swap(p_i, nb_less-1);
+        let mut pos_x: Vec<usize> = Vec::new();
+        let mut pos_y: Vec<usize> = Vec::new();
+        for i in 0..p_i {
+            if slice[i] > slice[p_i] {
+                pos_y.push(i);
+            }
+        }
+        for i in (p_i+1)..n {
+            if slice[i] <= slice[p_i] {
+                pos_x.push(i);
+            }
+        }
+        pos_x.reverse();
+        pos_y.reverse();
+
+        let mut it = pos_x.iter()
+            .zip(pos_y.iter())
+            .for_each(|(x, y)| slice.swap(*x, *y));
+
+        quick_sort(&mut slice[0..p_i], pivot);
+        quick_sort(&mut slice[(p_i+1)..n], pivot);
+        
+    }
+}
+
+fn merge_sort<T: PartialOrd + PartialEq>(slice: &mut [T]) {
+    
+}
+
 fn main() {
     println!("Hello, world!");
 }
 
 #[cfg(test)]
-mod tests {
+pub mod tests {
     use super::*;
 
-    #[allow(dead_code)]
+    #[derive(Debug)]
     enum TestData {
         VecI64(Vec<i64>),
         VecF64(Vec<f64>),
@@ -52,13 +96,11 @@ mod tests {
         VecStr(Vec<&'static str>),
     }
 
-    #[allow(dead_code)]
     enum QueryType {
         CheckEqual,
         CheckUnequal,
     }
 
-    #[allow(dead_code)]
     struct TestQuery {
         input: TestData,
         answer: TestData,
@@ -135,6 +177,14 @@ mod tests {
     fn test_bubble_sort() {
         for query in generate_queries() {
            test_sort_variant!(bubble_sort, query.input, query.answer, query.qtype);
+        }
+    }
+
+    #[test]
+    fn test_quick_sort_1() {
+        for query in generate_queries() {
+            fn custom_quick_sort<T: PartialOrd + PartialEq>(slice: &mut [T]) { quick_sort(slice, simple_pivot); }
+            test_sort_variant!(custom_quick_sort, query.input, query.answer, query.qtype);
         }
     }
 }
